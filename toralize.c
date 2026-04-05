d7b6667e4839a5963f7810af194b7000bbaa2401
Vaibhav Chimalgi <vchimu20@gmail.com>
2026-04-05 22:38:36 +0530

    first commit

---
+++ toralize.c
@@ -0,0 +1,24 @@
+/* toralize.c */
+#include "toralize.h "
+ 
+
+/*
+
+./toralize 1.2.3.4 80
+
+*/
+int main(int argc, char *argv[]){
+    char *host;
+    int port;
+
+    int (argc < 3){
+        fprintf(stderr, "Usage: %s <host> <port> \n", argv[0]); 
+        return -1;
+    }
+
+    host = argv[1];
+    port = atoi(argv[2]);
+
+
+     
+}
